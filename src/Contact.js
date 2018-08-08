c85e6d9132bc9296abd927ed7c2ae7d1b6b751b7
sonusasankan <sonusparsham@gmail.com>
2018-08-08 10:46:06 +0530

    initialize

--- src/Contact.js
+++ src/Contact.js
@@ -15,7 +15,7 @@ class Contact extends Component {
       <div className="card card-body mb-3">
         <h4>{name} <i onClick={ () => this.setState({ showContactInfo : !this.state.showContactInfo})} 
         className="fas fa-sort-down"></i>
-        <i className="fas fa-times" style={{float:'right', color:'red'}} onClick={this.onDeleteClick}></i>
+        <i className="fas fa-times" style={{cursor: 'pointer', float:'right', color:'red'}} onClick={this.onDeleteClick}></i>
         </h4>
         { showContactInfo ? (
                <ul className="list-group">
